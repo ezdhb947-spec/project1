bbc77a2622a7d51049240eab070394cb8906e610
ezdhb947-spec <ezdhb947@gmail.com>
2025-09-02 15:39:21 +0100

    Add files via upload

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 int main() {
-	int n;
-	printf("veuillez entrer lenombre de la lettre : ");
-	scanf("%d",&n);
-	switch (n) {
-		case 1 :printf("la lettre est une voyelle .");
+	int a,b;
+	printf("veuillez entrer l'annee : ");
+	scanf("%d",&a);
+	printf("convertir en  : \n");
+	printf("1- Mois \n");
+	printf("2- Jours \n");
+	printf("3- Heures \n");
+	printf("4- Minutes \n");
+	printf("5- Secondes \n");
+	scanf("%d",&b);
+	switch (b){
+		case 1:printf("%d mois",a*12);
 		break;
-		case 5 :printf("la lettre est une voyelle .");
+        case 2:printf("%d jours",a*365);
 		break;
-		case 9 :printf("la lettre est une voyelle .");
+		case 3:printf("%d heures",a*24);
 		break;
-		case 15 :printf("la lettre est une voyelle .");
+		case 4:printf("%d minutes",a*365*24*60);
 		break;
-		case 21 :printf("la lettre est une voyelle .");
+		case 5:printf("%d secondes",a*365*24*60*60);
 		break;
-		case 25 :printf("la lettre est une voyelle .");
-		break;
-		default : printf("la lettre est une consonne .");
-		
+		default:printf("choix invalide");
 	}
-	
-	}
+	return 0;
+}
